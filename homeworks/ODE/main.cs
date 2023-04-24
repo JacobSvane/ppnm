f6a4cbba2a4a52e8021c1d11041292fa8b21b9b7
Jacob <201705399@post.au.dk>
2023-04-24 10:50:51 +0200

    Solution to ODE part B Lotka-Volterra part

--- homeworks/ODE/main.cs
+++ homeworks/ODE/main.cs
@@ -46,18 +46,15 @@ public static void Oscillator_with_friction(){
 	}
 public static void Lotka_volterra(){
 	double a1=1.5, b1=1, c1=3, d1=1;
-	Func<double,vector,vector> F=delegate(double t, vector y){
-		double y1 = y[0], y2 = y[1];
-		return new vector(a1*y1-b1*y1*y2,-c1*y1+d1*y1*y2);
-		};
-	double a=0,d=10;
-	vector ya=new vector(0.0,1.0);
+	Func<double,vector,vector> F = (x,y) => new vector(a1*y[0]-b1*y[0]*y[1],-c1*y[1]+d1*y[0]*y[1]);
+	double a=0,d=15;
+	vector ya=new vector(10.0,5.0);
 	var xs=new genlist<double>();
 	var ys=new genlist<vector>();
 	double h=0.01,acc=1e-2,eps=1e-2;
 	ode.driver(F,a,ya,d,acc,eps,h,xs,ys);
 	var outfile1 = new System.IO.StreamWriter("Lotka_volterra.txt");
-	for(int i = 0; i<xs.size-1; i++){
+	for(int i = 0; i<xs.size-1; i ++){
 		outfile1.WriteLine($"{xs[i]} {ys[i][0]} {ys[i][1]}");
 	}
 	outfile1.Close();		
