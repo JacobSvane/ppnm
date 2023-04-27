d5e7d2bb9fa5cf1c5f37d67bf88e315b031bdf3f
Jacob <201705399@post.au.dk>
2023-04-27 11:06:21 +0200

    Adaptive integration part B done

--- homeworks/Adaptive_integration/Adaptive_integration.cs
+++ homeworks/Adaptive_integration/Adaptive_integration.cs
@@ -3,17 +3,24 @@ using static System.Console;
 using static System.Math;
 using System.Collections.Generic;
 public static partial class adaptiveintegrate{
-	
-public static double integrate(Func<double,double> f, double a, double b, double sigma=0.0001, double ε=0.0001, double f2 = Double.NaN, double f3= Double.NaN)
+public static int counter;
+public static int evals;
+public static double integrate(Func<double,double> f, double a, double b, double sigma=0.001, double ε=0.001, double f2 = Double.NaN, double f3= Double.NaN)
 {
 double h=b-a;
-if(Double.IsNaN(f2)){ f2=f(a+2*h/6); f3=f(a+4*h/6); } // first call, no points to reuse
-double f1=f(a+h/6), f4=f(a+5*h/6);
+if(Double.IsNaN(f2)){
+	counter = 0; evals = 2;
+	f2=f(a+2*h/6); f3=f(a+4*h/6); 
+	} // first call, no points to reuse
+double f1=f(a+h/6); double f4=f(a+5*h/6); evals +=2;
 double Q = (2*f1+f2+f3+2*f4)/6*(b-a); // higher order rule
 double q = (  f1+f2+f3+  f4)/4*(b-a); // lower order rule
 double err = Abs(Q-q);
 if (err <= sigma+ε*Abs(Q)) return Q;
-else return integrate(f,a,(a+b)/2,sigma/Sqrt(2),ε,f1,f2)+integrate(f,(a+b)/2,b,sigma/Sqrt(2),ε,f3,f4);
+else {
+	counter ++;
+	return integrate(f,a,(a+b)/2,sigma/Sqrt(2),ε,f1,f2)+integrate(f,(a+b)/2,b,sigma/Sqrt(2),ε,f3,f4);
+}
 }
 
 public static double errorfunction(double z){;
@@ -29,4 +36,9 @@ public static double errorfunction(double z){;
 		return 1-(2.0/Sqrt(PI))*integrate(f2,0,1);
 	}
 }
+public static double ccintegrate(Func<double,double> f, double a, double b, double del = 0.001, double eps = 0.001, double maxit = 100){
+	Func<double, double> g = (double theta) => (f((a+b)/2+(b-a)/2*Cos(theta))*Sin(theta)*(b-a)/2);
+	return integrate(g,0,PI,del,eps,maxit);
+}
+
 }//class
--- homeworks/Adaptive_integration/main.cs
+++ homeworks/Adaptive_integration/main.cs
@@ -6,6 +6,7 @@ class main{
 public static void Main(){
 	testintegrals();
 	Errorfunction();
+	CCtransformation();
 }//Main
 public static void testintegrals(){
 	Func<double, double> f1 = x => Sqrt(x);
@@ -15,20 +16,24 @@ public static void testintegrals(){
 	var outfile1 = new System.IO.StreamWriter("testintegrals.txt");
 	outfile1.WriteLine("Testing integrals within the expected accuracy which is given as 0.001 in this case");
 	outfile1.WriteLine("The integrals are tested using Math.Abs(clac-known) <= 0.001. If this is true, the integral is printed");
-	if (Abs(adaptiveintegrate.integrate(f1,0,1) - (double) 2/3) <= 0.001)
+	if (Abs(adaptiveintegrate.integrate(f1,0,1) - (double) 2/3) <= 0.001){
 		outfile1.WriteLine($"∫\x2080\x00B9 dx √(x) = {adaptiveintegrate.integrate(f1,0,1)}, expected value = 2/3");
+	outfile1.WriteLine($"The number of subdivisions was {adaptiveintegrate.counter} with {adaptiveintegrate.evals} evaluations\n");}
 	else
 		outfile1.WriteLine($"The integral is not within the accuracy goal");
-if (Abs(adaptiveintegrate.integrate(f2,0,1) - (double) 2) <= 0.001)
+if (Abs(adaptiveintegrate.integrate(f2,0,1) - (double) 2) <= 0.001){
 		outfile1.WriteLine($"∫\x2080\x00B9 dx 1/√(x) = {adaptiveintegrate.integrate(f2,0,1)}, expected value = 2");
+outfile1.WriteLine($"The number of subdivisions was {adaptiveintegrate.counter} with {adaptiveintegrate.evals} evaluations\n");}
 	else
 		outfile1.WriteLine($"The integral is not within the accuracy goal");
-if (Abs(adaptiveintegrate.integrate(f3,0,1) - (double) PI) <= 0.001)
+if (Abs(adaptiveintegrate.integrate(f3,0,1) - (double) PI) <= 0.001){
 		outfile1.WriteLine($"∫\x2080\x00B9 dx 4*√(1-x²) = {adaptiveintegrate.integrate(f3,0,1)}, expected value = {PI}");
+outfile1.WriteLine($"The number of subdivisions was {adaptiveintegrate.counter} with {adaptiveintegrate.evals} evaluations\n");}
 	else
 		outfile1.WriteLine($"The integral is not within the accuracy goal");
-if (Abs(adaptiveintegrate.integrate(f4,0,1) - (double) -4) <= 0.001)
+if (Abs(adaptiveintegrate.integrate(f4,0,1) - (double) -4) <= 0.001){
 		outfile1.WriteLine($"∫\x2080\x00B9 dx ln(x)/√(x) = {adaptiveintegrate.integrate(f4,0,1)}, expected value = -4");
+outfile1.WriteLine($"The number of subdivisions was {adaptiveintegrate.counter} with {adaptiveintegrate.evals} evaluations\n");}
 	else
 		outfile1.WriteLine($"The integral is not within the accuracy goal");
 	outfile1.Close();	
@@ -40,5 +45,16 @@ public static void Errorfunction(){
 		outfile2.WriteLine($"{i} {adaptiveintegrate.errorfunction(i)}");
 	}
 	outfile2.Close();
+	}
+public static void CCtransformation(){
+	var outfile3 = new System.IO.StreamWriter("CCtransformation.txt");
+	Func<double, double> f5 = x => 1/Sqrt(x);
+	Func<double, double> f6 = x => Log(x)/Sqrt(x);
+	outfile3.WriteLine($"∫\x2080\x00B9 dx 1/√(x) = {adaptiveintegrate.ccintegrate(f5,0,1)}, expected value = 2");
+	outfile3.WriteLine($"The number of subdivisions was {adaptiveintegrate.counter} with {adaptiveintegrate.evals} evaluations");
+	outfile3.WriteLine($"∫\x2080\x00B9 dx ln(x)/√(x) = {adaptiveintegrate.ccintegrate(f6,0,1)}, expected value = -4");
+	outfile3.WriteLine($"The number of subdivisions was {adaptiveintegrate.counter} with {adaptiveintegrate.evals} evaluations");
+	outfile3.WriteLine($"\nThe routine now solves the integrals faster. The integrals were also solved using scipy in python.\n∫\x2080\x00B9 dx 1/√(x) was solved in 6 subdivisions and 231 evaluations while ∫\x2080\x00B9 dx ln(x)/√(x) was solved in 8 subdivisions and 315 evaluations.\nThus it is found that the function made in c# solves ∫\x2080\x00B9 dx 1/√(x) in fewer divisions and evaluations while ∫\x2080\x00B9 dx ln(x)/√(x) is solved using fewer evaluations but more subdivisions.");
+	outfile3.Close();
 	}
 }//main
